707ac145c0d8f82820aaa24a7b25c02a47a15d01
namba-hrk <hiroki.namba@northdetail.co.jp>
2024-07-16 09:18:57 +0900

    scss-edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@use "src/styles/index.scss" as *;',
+          },
+        },
+      },
+    },
 });
